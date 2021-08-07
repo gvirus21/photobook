edf8111d1210e3d4221fd4ef27f228bb8d87299b
gourav <gvirus21yt@gmail.com>
2021-08-07 12:11:04 +0530

    farmer motion animation added

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UseFirestore from "../hooks/UseFirestore";
+import { motion } from "framer-motion";
 
 const Grid = ({ setSelectedImg }) => {
   const { docs } = UseFirestore("images");
@@ -9,14 +10,22 @@ const Grid = ({ setSelectedImg }) => {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
+          <motion.div
+            layout
+            whileHover={{ opacity: 1 }}
             className="img-wrap"
             key={doc.id}
             onClick={() => setSelectedImg(doc.url)}
           >
-            <img src={doc.url} alt="pic" />
+            <motion.img
+              src={doc.url}
+              alt="pic"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
             {console.log(docs.url)}
-          </div>
+          </motion.div>
         ))}
       {console.log(docs)}
     </div>
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import UseStorage from "../hooks/UseStorage";
-
+import { motion } from "framer-motion";
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = UseStorage(file);
 
@@ -12,7 +12,11 @@ const ProgressBar = ({ file, setFile }) => {
 
   return (
     <div className="progress-bar-container">
-      <div className="progress-bar" style={{ width: progress + "%" }}></div>
+      <motion.div
+        className="progress-bar"
+        initial={{ width: 0 }}
+        animate={{ width: progress + "%" }}
+      ></motion.div>
     </div>
   );
 };
